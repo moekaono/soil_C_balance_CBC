663ad61579b55c48b615ca7293b8b4ebf2747455
Moeka Ono <90373346+moekaono@users.noreply.github.com>
2025-01-30 09:53:00 -0600

    Update char_lmer.R

--- Codes/char_lmer.R
+++ Codes/char_lmer.R
@@ -48,12 +48,28 @@ char_plmean_imp <-
   arrange(Stands, Plot) 
 
 # plot - temporal charcoal production
-ggplot(char_plmean_imp %>% filter(!is.na(MY))) +
+char_plmean_imp %>% filter(!is.na(MY)) %>% 
+  mutate(
+    date = as.Date.character(paste0("01-", substring(MY, 1, 3), "-20", substring(MY, 4, 5)), 
+                             format = "%d-%b-%Y")
+    ) %>% 
+  ggplot() +
   geom_boxplot(
-    aes(x = factor(MY, MY_order), y = char_per_area_mean, col = Stands)
+    aes(x = date, y = char_per_area_mean, col = Stands, group = interaction(date, Stands))
     ) +
   theme_classic() +
-  xlab("MonthYear") + ylab("Charcoal (g/cm2)")
+  #xlab("MonthYear") + 
+  ylab(expression("Charcoal mass (g m"^-2~")")) +
+  scale_x_date(date_breaks = "3 months", date_labels = "%b-%y") +
+  theme(
+    axis.title.x = element_blank(), 
+    axis.text.x = element_text(size = 20),
+    axis.title.y = element_text(size = 20),
+    axis.text.y = element_text(size = 20),
+    legend.title = element_text(size = 20),
+    legend.text = element_text(size = 20)
+  )
+
   
 
 # Q1 - are there monthly diffs
@@ -87,6 +103,29 @@ anova(char_AB_model)
 char_AB_posthoc <- emmeans::emmeans(char_AB_model, ~ MY)
 summary(char_AB_posthoc)
 lsmeans::lsmeans(char_AB_model, pairwise ~ MY, adjust = "tukey")
+
+
+# AB - before after fires
+char_test_AB_fire <-
+  char_plmean_yr %>% 
+  filter(Stands == "AB") %>% 
+  filter(MY == "Sep20" | MY == "Oct20" |MY == "Nov20" )
+
+char_AB_fire_model <- 
+  lmerTest::lmer(char_per_area_mean ~ MY + (1|Plot), data = char_test_AB_fire)
+anova(char_AB_fire_model)
+
+char_AB_fire_posthoc <- emmeans::emmeans(char_AB_fire_model, ~ MY)
+summary(char_AB_fire_posthoc)
+lsmeans::lsmeans(char_AB_fire_model, pairwise ~ MY, adjust = "tukey")
+
+
+ggplot(char_test_AB_fire) +
+  geom_boxplot(aes(x = factor(MY, level = c("Sep20", "Oct20", "Nov20")), char_per_area_mean)) + 
+  theme_classic() +
+  xlab("MonthYear") +
+  ylab("Charcoal (g/m2)") +
+  ggtitle("Before and after a fire in AB")
 
 # IB
 char_test_IB <-
@@ -142,6 +181,11 @@ library(stats)
 char_yr_anova <- aov(mean ~ Stands, data = char_mean_yr_plot)
 summary(char_yr_anova)
 
+ggplot(char_mean_yr_plot) +
+  geom_boxplot(aes(Stands, mean)) +
+  theme_classic() +
+  ylab("Charcoal (g/m2)") +
+  ggtitle("Annual mean")
 
 
 # stand mean
